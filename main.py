6ccc51e3379a770651a1281771fa429f3154391e
42willow <70784752+42Willow@users.noreply.github.com>
2024-01-08 20:28:49 +1100

    refactor: change ping command to slash command

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# Sync command tree
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+    await bot.tree.sync()
+
+@bot.tree.command(name="ping", description="Ping ARRB :3")
+async def slash_command(interaction:discord.Interaction):
+    await interaction.response.send_message("Pong!")
 
 bot.run(token)
