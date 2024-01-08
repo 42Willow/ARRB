dd6777ff372c2ce82cdabfd4dd46a4b12918ff1f
42willow <70784752+42Willow@users.noreply.github.com>
2024-01-09 08:53:46 +1100

    feat: reporting to hard-coded channel

--- main.py
+++ main.py
@@ -1,24 +1,73 @@
+from typing import Optional
 import discord
-from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv, find_dotenv
 import os
 
 load_dotenv()  # Load variables from .env file
+# Your secret Discord token, don't share this with anyone!
+TOKEN = os.getenv('DISCORD_TOKEN')
+# Your guild where you want to sync commands on each startup
+GUILD_ID = discord.Object(id=int(os.getenv('DISCORD_GUILD_ID')))
 
-token = os.getenv('DISCORD_TOKEN')
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=GUILD_ID)
+        await self.tree.sync(guild=GUILD_ID)
 
 intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+client = MyClient(intents=intents)
 
-# Sync command tree
-@bot.event
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}")
-    await bot.tree.sync()
+    print(f"Logged in as {client.user.name}")
 
-@bot.tree.command(name="ping", description="Ping ARRB :3")
-async def slash_command(interaction:discord.Interaction):
-    await interaction.response.send_message("Pong!")
+@client.tree.command()
+async def ping(interaction:discord.Interaction):
+    """Ping ARRB :3"""
+    emoji = client.get_emoji(1166270365627056138)
+    await interaction.response.send_message(f'Pong! {emoji}')
 
-bot.run(token)
+class ReportView(discord.ui.View):
+    def __init__(self, message: discord.Message):
+        super().__init__()
+        self.message = message  # Store the message
+        self.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
+        
+    @discord.ui.button(label='Delete', style=discord.ButtonStyle.red)
+    async def delete(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.message.delete()
+        await interaction.response.send_message(f"Reported message deleted by {interaction.user.mention}")
+        button.disabled = True
+        self.stop()
+        await interaction.message.edit(view=self)
+
+@client.tree.context_menu(name='Report to Moderators')
+async def report_message(interaction: discord.Interaction, message: discord.Message):
+    # We're sending this response message with ephemeral=True, so only the command executor can see it
+    await interaction.response.send_message(f"Thanks for reporting this message by {message.author.mention} to our moderators.", ephemeral=True)
+    # Handle report by sending it into a log channel
+    log_channel = interaction.guild.get_channel(1117569068220825741)
+    embed = discord.Embed(title='Reported Message')
+    if message.content:
+        embed.description = message.content
+    embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+    embed.timestamp = message.created_at
+    view = ReportView(message)
+    await log_channel.send(embed=embed, view=view)
+
+# @client.tree.command()
+# @app_commands.describe(member="Who's social status do I check?")
+# async def profile(interaction: discord.Interaction, member: Optional[discord.Member] = None):
+#     """Sends social status of a member"""
+#     # If no member is explicitly provided then we use the command user here
+#     member = member or interaction.user
+
+#     # The format_dt function formats the date time into a human readable representation in the official client
+#     await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+client.run(TOKEN)
